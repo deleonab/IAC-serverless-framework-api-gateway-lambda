c5d8adcc0b0541716a9a45b0ef7a631c53ed9a8a
Dele Onabowu <delesodade@hotmail.com>
2022-08-30 00:30:54 +0100

    updated code for serverless.yml

--- aws-node-http-api-project/src/addTodo.js
+++ aws-node-http-api-project/src/addTodo.js
@@ -1,11 +1,12 @@
 "use strict";
+const { v4 } = require("uuid");
 
 const addTodo = async (event) => {
 
   const { todo } = JSON.parse(event.body);
   const createdAt = new Date();
   const id = v4();
-  
+
   return {
     statusCode: 200,
     body: JSON.stringify(
