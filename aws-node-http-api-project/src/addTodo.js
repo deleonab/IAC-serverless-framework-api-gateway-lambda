c6bad4d815fc96ada92eaf997c55431da9633d1e
Dele Onabowu <delesodade@hotmail.com>
2022-08-30 18:16:38 +0100

    updated code for serverless.yml

--- aws-node-http-api-project/src/addTodo.js
+++ aws-node-http-api-project/src/addTodo.js
@@ -1,24 +1,31 @@
 "use strict";
 const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
 
 const addTodo = async (event) => {
-
+  const dynamodb = AWS.DynamoDB.DocumentClient();
   const { todo } = JSON.parse(event.body);
   const createdAt = new Date();
   const id = v4();
 
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    completed: false
+  }
+
+  await dynamodb.put({
+    TableName: TodoTable,
+    Item: {newTodo}
+ 
+ })
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(newTodo),
   };
 };
 module.exports = {
-  handler:hello
+  handler:addTodo
 }
